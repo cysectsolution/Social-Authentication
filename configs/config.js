210d834201d3bba51494a010d589ab4e2d57fe93
Denniskamau <denniskamau3@gmail.com>
2017-10-17 10:38:36 +0300

    changed .gitignore file

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    
+        "facebookAuth":{
+            'clientID':'225778091162634',
+            'clientSecret':'768c0edf172d7d4451ca740436e53e31',
+            'callbackURL':'http://localhost:3000/auth/facebook/callback'
+        }
+        
+    }
---
+++ configs/passport.js
@@ -0,0 +1,16 @@
+var config = require('./config');
+var FacebookStrategy = require ('passport-facebook');
+var passport = require ('passport');
+var User = require('../models/users')
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebookAuth.clientID,
+    clientSecret: config.facebookAuth.clientSecret,
+    callbackURL: config.facebookAuth.callbackURL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
